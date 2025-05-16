1cdcdc6f3a920a24b8536c79701fe221d653e44b
Avinash Gupta <guptaavinash302@gmail.com>
2025-05-16 10:50:27 +0530

    react + taiwlind setup

---
+++ sa-portal/src/App.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import Carousel from "./Components/Carousel";
+import Navbar from "./Components/Navbar";
+const App = () => {
+ 
+
+  return (
+    <>
+      <Navbar />
+      <Carousel />
+    </>
+  );
+};
+
+export default App;
